3f42e261800b8c4d82c444d07ab69d27445fbd20
dedZombie <hemicar92@gmail.com>
2019-08-14 22:37:41 +0300

    Finished greetings section of a homepage
    - Added webpack loader for fonts
    
    - Added all the font files, and some static content

--- webpack.common.js
+++ webpack.common.js
@@ -12,12 +12,22 @@ module.exports = {
         use: ["html-loader"]
       },
       {
-        test: /\.(svg|png|jpg|gif)$/,
+        test: /\.(png|jp(e*)g|gif)$/,
         use: {
           loader: "file-loader",
           options: {
             name: "[name].[hash].[ext]",
             outputPath: "img"
+          }
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "fonts"
           }
         }
       }
