ef682cffd419033df3db80f89dbc2cdd6e45fa8b
dedZombie <hemicar92@gmail.com>
2019-08-15 03:02:48 +0300

    Finished mobile view of Homepage
    - Added new fonts
    
    - Added library for hamburger menu button
    
    - Added some global utility classes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 import "../sass/main.scss";
+
+var hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener("click", function() {
+  hamburger.classList.toggle("is-active");
+});
